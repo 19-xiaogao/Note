761b30aa06ec7d7275d25110a2ecef1d2d357a70
xiaolong <longjiuwei999@163.com>
2021-11-26 15:13:29 +0800

    编写go文档

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -10,7 +10,7 @@ module.exports = {
     nav: [
       { text: "首页", link: "/" },
       { text: "前端", link: "/web/" },
-      { text: "后端", link: "/back_end/" },
+      { text: "后端", link: "/backend/" },
       { text: "计算机网络", link: "/computerNetwork/" },
       { text: "密码学", link: "/cryptography/" },
       { text: "运维", link: "/operations/" },
@@ -22,6 +22,26 @@ module.exports = {
           title: "js基础",
           sidebarDepth: 0,
           children: ["/web/baseJs/baseJs"],
+        },
+      ],
+      "/backend/": [
+        {
+          title: "GoLang",
+          children: [
+            {
+              title: "基础",
+              children: [
+                "/backend/golang/base/base",
+                "/backend/golang/base/基本数据类型",
+              ],
+            },
+          ],
+        },
+      ],
+      "/cryptography/": [
+        {
+          title: "密码学",
+          sidebarDepth: 0,
         },
       ],
     },
